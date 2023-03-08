7193635fe2d195ee87eca8b271f16b79bdccefdb
Noah Krüger <extern.krueger_noah-ben@allianz.com>
2023-03-08 13:22:33 +0100

    Aufgabe Datatypes Declarations, adds results

--- src/datatypes/Declaration.java
+++ src/datatypes/Declaration.java
@@ -1,13 +1,13 @@
 package datatypes;
 
-// a) 5 int
-// b) 5.0 double
+// a) 5 int 5
+// b) 5.0 double 5
 // c) 4L long 4
 // d) 3 + 9 int 12
-// e) 2 + 6.0 double
-// f) 5 + 3.0f float
-// g) 3d + 1.0f double
-// h) 6L * 3 long
-// i) 7 / 2 int
-// j) 9 / 2.0 double
-// k) int i = 2; i--; int
+// e) 2 + 6.0 double 8
+// f) 5 + 3.0f float 8
+// g) 3d + 1.0f double 4
+// h) 6L * 3 long 18
+// i) 7 / 2 int 3.5
+// j) 9 / 2.0 double 4.5
+// k) int i = 2; i--; int 1
