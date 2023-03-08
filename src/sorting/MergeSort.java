16757e4acf6822d591da4938a5805368efd692e0
Noah Krüger <extern.krueger_noah-ben@allianz.com>
2023-03-08 15:20:06 +0100

    implements my Attempt at Merge Sort with Comments for a better understanding(for myself)

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class MergeSort {
 
+    // die sort methode teilt rekursiv den input array in der hälfte und merged sie
     public int[] sort(int[] numbers) {
+
+        // Falls die Länge des arrays größer als 1 ist, teilt er ihn in der Hälfte und sortiert jede Hälfte für sich
         if (numbers.length > 1) {
             int mid = numbers.length / 2;
             int[] left = Arrays.copyOfRange(numbers, 0, mid);
@@ -12,17 +15,26 @@ public class MergeSort {
             sort(left);
             sort(right);
             merge(numbers, left, right);
+
+            // Gibt den Array nach dem Merge aus für ein besseres Verständnis für mich :)
             System.out.print("After merge: ");
             for (int i = 0; i < numbers.length; i++) {
                 System.out.print(numbers[i] + " ");
             }
             System.out.println();
         }
+
+        // Returned den sortierten Array
         return numbers;
     }
 
+    // Die Merge Methode kombiniert zwei sortierte Arrays
     private void merge(int[] numbers, int[] left, int[] right) {
+
+        // Initialisierung von countern für den linken, rechten und kombinierten bzw. merged Array
         int i = 0, j = 0, k = 0;
+
+        // Iteriert über beode Arrays und added das kleinste Element zum merged Arrya
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
                 numbers[k] = left[i];
@@ -33,11 +45,15 @@ public class MergeSort {
             }
             k++;
         }
+
+        // Added die verbleibenden Elemente vom linken Array in den merged Array
         while (i < left.length) {
             numbers[k] = left[i];
             i++;
             k++;
         }
+
+        // Added die übrigen Elemente vom rechten Array zum merged Array
         while (j < right.length) {
             numbers[k] = right[j];
             j++;
