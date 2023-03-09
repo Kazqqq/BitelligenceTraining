32ef2a1844fe43b8f7cf68d370939ae7d87a179b
Noah Krüger <extern.krueger_noah-ben@allianz.com>
2023-03-09 16:19:38 +0100

    implements GuessMe Game

---
+++ src/guessinggame/GuessMe.java
@@ -0,0 +1,25 @@
+package guessinggame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessMe {
+
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        Random random = new Random();
+        int zufallsZahl = random.nextInt(100) + 1;
+        System.out.println("Willkommen beim Zahlen raten, bereit zu spielen? Dann trage deine Zahl ein: ");
+        int geraten = eingabe.nextInt();
+
+        if (geraten == zufallsZahl) {
+            System.out.println("Wow, dein Guess war korrekt - du hast gewonnen!");
+        } else if (geraten < zufallsZahl) {
+            System.out.println("Haha, leider daneben, deine Zahl ist größer");
+        } else {
+            System.out.println("Garnicht so schlecht für einen Verlierer, deine Zahl war kleiner als die Glückszahl");
+        }
+        System.out.println("Die Glückszahl war: " + zufallsZahl);
+    }
+
+}
