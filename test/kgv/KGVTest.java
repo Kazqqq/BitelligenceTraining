c307e486f0ad08518dad47cd561cf9245ffc235b
Noah Krüger <extern.krueger_noah-ben@allianz.com>
2023-03-09 15:07:24 +0100

    implements kgvTEST

--- test/kgv/KGVTest.java
+++ test/kgv/KGVTest.java
@@ -10,8 +10,8 @@ public class KGVTest {
         int a = 15;
         int b = 9;
         int expectedKGV = 45;
-        int actualKGV = KGV.kgv(a, b);
-        assertEquals(expectedKGV, actualKGV);
+        int realKGV = KGV.kgv(a, b);
+        assertEquals(expectedKGV, realKGV);
     }
 }
 
