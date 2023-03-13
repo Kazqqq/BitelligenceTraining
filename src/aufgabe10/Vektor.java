3e94325535864c94b05a359545965f22f2d5b4b9
Noah Krüger <extern.krueger_noah-ben@allianz.com>
2023-03-13 14:07:16 +0100

    implements Vektor and vektor calc Methods

---
+++ src/aufgabe10/Vektor.java
@@ -0,0 +1,66 @@
+package aufgabe10;
+
+public class Vektor {
+    private double x, y, z;
+
+    public double getX() {
+        return x;
+    }
+
+    public Vektor setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Vektor setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public Vektor setZ(double z) {
+        this.z = z;
+        return this;
+    }
+
+    public Vektor(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vektor addieren(Vektor vektor) {
+        return new Vektor(x + vektor.x, y + vektor.y, z + vektor.z);
+    }
+
+    public Vektor subtrahieren(Vektor vektor) {
+        return new Vektor(x - vektor.x, y - vektor.y, z - vektor.z);
+    }
+
+    public Vektor dividieren(double d) {
+        return new Vektor(x/d,y/d,z/d);
+    }
+
+    public Vektor skalarMultiply(double s) {
+        return new Vektor(s * x, s + y, s + z);
+    }
+
+    public Vektor skalarProdukt(Vektor vektor) {
+        return new Vektor(x * vektor.x, y * vektor.y, z * vektor.z);
+    }
+
+    public Vektor kreuzProdukt(Vektor vektor) {
+        return new Vektor(y * vektor.z - z * vektor.y, z * vektor.x - x * vektor.z, x * vektor.y - y * vektor.x);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+}
